524e9c70200e98fbc98f2b9477b892a67d7a357c
Alec Mitchell <alecpm@gmail.com>
2023-02-20 17:06:49 -0800

    Add some additional dict compatibilty methods (without `__contains__` `in` is deadlocking for some reason).

--- Products/BeakerSessionDataManager/sessiondata.py
+++ Products/BeakerSessionDataManager/sessiondata.py
@@ -222,13 +222,16 @@ class BeakerSessionDataObject(Implicit):
     # dict API compatibility used by some plone addons e.g. collective.z3cform.wizard
 
     def has_key(self, key):
-        return key in self.session.keys()
+        return key in self.session
 
-    def get(self, key):
-        return self.session.get(key)
+    def get(self, key, default=None):
+        return self.session.get(key, default)
 
     def __getitem__(self, key):
         return self.get(key)
+
+    def __contains__(self, key):
+        return key in self.session
 
 
 InitializeClass(BeakerSessionDataObject)
